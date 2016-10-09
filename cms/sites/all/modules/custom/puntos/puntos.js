83972fe1710f7382c08942c0edc8dbe6a4ed89b0
dsabolo <diego.sabolo@gmail.com>
2016-10-09 19:22:33 -0300

    Hide Titles

---
+++ cms/sites/all/modules/custom/puntos/puntos.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(
+  function(){
+    jQuery('#canjeo-puntos-node-form #edit-title').val("Registro Canjeo de Puntos");
+    jQuery('#canjeo-puntos-node-form .form-item-title').hide();
+    jQuery('#puntos-node-form #edit-title').val("Registro Suma de Puntos");
+    jQuery('#puntos-node-form .form-item-title').hide();
+  }
+);
