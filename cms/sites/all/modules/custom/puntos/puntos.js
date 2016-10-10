610b8973583c962d40b9fe0091734d4483549b96
dsabolo <diego.sabolo@gmail.com>
2016-10-09 22:05:21 -0300

    Premios y mejoras generales

--- cms/sites/all/modules/custom/puntos/puntos.js
+++ cms/sites/all/modules/custom/puntos/puntos.js
@@ -2,6 +2,10 @@ jQuery(document).ready(
   function(){
     jQuery('#canjeo-puntos-node-form #edit-title').val("Registro Canjeo de Puntos");
     jQuery('#canjeo-puntos-node-form .form-item-title').hide();
+    jQuery('#canjeo-puntos-node-form .form-item-field-puntos-und-0-value').hide();
+    jQuery('#canjeo-puntos-node-form .form-item-field-descripci-n-und-0-value').hide();
+    jQuery('.page-node-add-puntos #page-title').html("Sumar Puntos");
+    jQuery('.page-node-add-canjeo-puntos #page-title').html("Canjear Puntos");
     jQuery('#puntos-node-form #edit-title').val("Registro Suma de Puntos");
     jQuery('#puntos-node-form .form-item-title').hide();
   }
